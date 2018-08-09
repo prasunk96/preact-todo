1741bf5803b9873cb1fc4ab7046e311142173d52
Avadhut N Prabhudesai <GS-1489@GSLAB.COM>
2018-08-09 14:21:24 +0530

    code cleaned up. npm packages added.

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,6 @@ import Header from './header';
 
 // Code-splitting is automated for routes
 import Home from '../routes/home';
-import Profile from '../routes/profile';
 
 export default class App extends Component {
 	
@@ -23,8 +22,6 @@ export default class App extends Component {
 				<Header />
 				<Router onChange={this.handleRoute}>
 					<Home path="/" />
-					<Profile path="/profile/" user="me" />
-					<Profile path="/profile/:user" />
 				</Router>
 			</div>
 		);
